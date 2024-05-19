dcc83d3c29438385d6c5df9960f255ea54e6a14e
sho5081857 <masashi713416@gmail.com>
2024-05-19 22:01:16 +0900

    Change callback handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -207,7 +207,9 @@ export async function authenticate(
   formData: FormData,
 ) {
   try {
-    await signIn('credentials', formData);await signIn('credentials', formData);
+    await signIn('credentials', formData, {
+      callbackUrl: '/dashboard',
+    });
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
